2201384ed4ea2adb53a32eb53b4063805097e74b
dertfin0 <dertfin@gmail.com>
2026-01-31 20:49:49 +0300

    Add lowercase support Closes #1

--- govnoscript.py
+++ govnoscript.py
@@ -32,32 +32,32 @@ def interpret(lines: list):
         if line0[0].startswith("#"):
             continue
 
-        if line0[0] == "MOVETO" and len(line0) == 4:
+        if line0[0].lower() == "moveto" and len(line0) == 4:
             gui.moveTo(int(line0[1]), int(line0[2]), float(line0[3]))
-        elif line0[0] == "CLICK" and len(line0) == 2:
-            if line0[1] == "LEFT":
+        elif line0[0].lower() == "click" and len(line0) == 2:
+            if line0[1].lower() == "left":
                 gui.click()
-            elif line0[1] == "RIGHT":
+            elif line0[1].lower() == "right":
                 gui.rightClick()
-            elif line0[1] == "MIDDLE":
+            elif line0[1].lower() == "middle":
                 gui.middleClick()
             else:
                 print(f"Unknown mouse button [{line0[1]}]")
-        elif line0[0] == "WAIT" and len(line0) == 2:
+        elif line0[0].lower() == "wait" and len(line0) == 2:
             time.sleep(float(line0[1]))
-        elif line0[0] == "WRITE" and len(line0) >= 3:
+        elif line0[0].lower() == "write" and len(line0) >= 3:
             text = " ".join(line0[2:])
             kb.write(text, float(line0[1]))
-        elif line0[0] == "WRITE_BY_KEYBOARD" and len(line0) >= 3:
+        elif line0[0].lower() == "write_by_keyboard" and len(line0) >= 3:
             text = " ".join(line0[2:]).lower()
             for char in text:
                 time.sleep(float(line0[1]))
                 kb.press(char)
                 time.sleep(0.05)
                 kb.release(char)
-        elif line0[0] == "PRESS" and len(line0) == 2:
+        elif line0[0].lower() == "press" and len(line0) == 2:
             kb.press(line0[1])
-        elif line0[0] == "RELEASE" and len(line0) == 2:
+        elif line0[0].lower() == "release" and len(line0) == 2:
             kb.release(line0[1])
         else:
             print(f"Unknown command [{line}]")
