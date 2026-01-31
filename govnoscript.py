cbd585d76edcab047d3eef215747b3cd8eacf833
dertfin0 <dertfin@gmail.com>
2026-01-31 21:08:18 +0300

    Use match-case instead of if

--- govnoscript.py
+++ govnoscript.py
@@ -32,32 +32,55 @@ def interpret(lines: list):
         if line0[0].startswith("#"):
             continue
 
-        if line0[0].lower() == "moveto" and len(line0) == 4:
-            gui.moveTo(int(line0[1]), int(line0[2]), float(line0[3]))
-        elif line0[0].lower() == "click" and len(line0) == 2:
-            if line0[1].lower() == "left":
-                gui.click()
-            elif line0[1].lower() == "right":
-                gui.rightClick()
-            elif line0[1].lower() == "middle":
-                gui.middleClick()
-            else:
-                print(f"Unknown mouse button [{line0[1]}]")
-        elif line0[0].lower() == "wait" and len(line0) == 2:
-            time.sleep(float(line0[1]))
-        elif line0[0].lower() == "write" and len(line0) >= 3:
-            text = " ".join(line0[2:])
-            kb.write(text, float(line0[1]))
-        elif line0[0].lower() == "write_by_keyboard" and len(line0) >= 3:
-            text = " ".join(line0[2:]).lower()
-            for char in text:
+        match line[0].lower():
+            case "moveto":
+                if len(line0) != 4:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
+                gui.moveTo(int(line0[1]), int(line0[2]), float(line0[3]))
+            case "click":
+                if len(line0) != 2:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
+                match line0[1].lower():
+                    case "left":
+                        gui.leftClick()
+                    case "right":
+                        gui.rightClick()
+                    case "middle":
+                        gui.middleClick()
+                    case _:
+                        print(f"Unknown mouse button [{line0[1]}]")
+            case "wait":
+                if len(line0) != 2:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
                 time.sleep(float(line0[1]))
-                kb.press(char)
-                time.sleep(0.05)
-                kb.release(char)
-        elif line0[0].lower() == "press" and len(line0) == 2:
-            kb.press(line0[1])
-        elif line0[0].lower() == "release" and len(line0) == 2:
-            kb.release(line0[1])
-        else:
-            print(f"Unknown command [{line}]")
+            case "write":
+                if len(line0) < 3:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
+                text = " ".join(line0[2:])
+                kb.write(text, float(line0[1]))
+            case "write_by_keyboard":
+                if len(line0) < 3:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
+                text = " ".join(line0[2:]).lower()
+                for char in text:
+                    time.sleep(float(line0[1]))
+                    kb.press(char)
+                    time.sleep(0.05)
+                    kb.release(char)
+            case "press":
+                if len(line0) != 2:
+                    print(f"Syntax error in line [{lines.index(line)+1}]")
+                    continue
+                kb.press(line0[1])
+            case "release":
+                if len(line0) != 2:
+                    print(f"Syntax error in line [{lines.index(line) + 1}]")
+                    continue
+                kb.release(line0[1])
+            case _:
+                print(f"Unknown command [{line}]")
